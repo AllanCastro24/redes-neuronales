d2c6b4dfebb578a6fc4d520a6fdb62bb82fa0db7
AllanCastro24 <alan.castro.1226.ac@gmail.com>
2022-06-21 21:37:42 -0600

    proyecto final

--- Proyecto_final/proyecto_grupal/src/com/heatonresearch/book/introneuralnet/ch5/xor/XOR.java
+++ Proyecto_final/proyecto_grupal/src/com/heatonresearch/book/introneuralnet/ch5/xor/XOR.java
@@ -2,9 +2,9 @@ package com.heatonresearch.book.introneuralnet.ch5.xor;
 
 import com.heatonresearch.book.introneuralnet.neural.feedforward.FeedforwardLayer;
 import com.heatonresearch.book.introneuralnet.neural.feedforward.FeedforwardNetwork;
-import com.heatonresearch.book.introneuralnet.neural.feedforward.train.anneal.NeuralSimulatedAnnealing;
 import java.io.IOException;
 import com.heatonresearch.book.introneuralnet.common.ReadCSV;
+import com.heatonresearch.book.introneuralnet.neural.feedforward.train.genetic.TrainingSetNeuralGeneticAlgorithm;
 
 public class XOR {
 	public static int filas;
@@ -13,6 +13,8 @@ public class XOR {
 	public static void main(final String args[]) throws IOException {
 		final ReadCSV archivo = new ReadCSV("entrada.csv");		
 		filas = archivo.rows("entrada.csv");
+                final ReadCSV archivo2 = new ReadCSV("train.csv");		
+		filas = archivo2.rows("train.csv");
 		final FeedforwardNetwork network = new FeedforwardNetwork();
 		network.addLayer(new FeedforwardLayer(6));          					
 		network.addLayer(new FeedforwardLayer(5));								
@@ -38,30 +40,63 @@ public class XOR {
 			csv[j][6] = ideal;
 			j += 1;
 		}
+                
+                j = 0;	
+                //guarda archivos en un matriz
+		while (archivo2.next()) {														
+			final double p1 = Double.parseDouble(archivo2.get("pregunta1"));
+			final double p2 = Double.parseDouble(archivo2.get("pregunta2"));
+			final double p3 = Double.parseDouble(archivo2.get("pregunta3"));
+			final double p4 = Double.parseDouble(archivo2.get("pregunta4"));
+			final double p5 = Double.parseDouble(archivo2.get("pregunta5"));
+			final double p6 = Double.parseDouble(archivo2.get("pregunta6"));
+			//final double ideal = Double.parseDouble(archivo2.get("ideal"));
+			
+			csv2[j][0] = p1;
+			csv2[j][1] = p2;
+			csv2[j][2] = p3;
+			csv2[j][3] = p4;
+			csv2[j][4] = p5;
+			csv2[j][5] = p6;
+			//csv[j][6] = ideal;
+			j += 1;
+		}
 		//se manda a llamar para guardar en otra matriz
 		final double xorData[][] = getGrid2();
+                final double xorData2[][] = getGrid3();
                 final double xorIdeal[][] = getIdeal();
-        //se manda a entrenar con el metodo del recocio
-                final NeuralSimulatedAnnealing train = new NeuralSimulatedAnnealing(network, xorData, xorIdeal, 10, 2, 50000);
-		//final Train train = new Backpropagation(network, xorData, xorIdeal,0.7, 0.9);
+                //se manda a entrenar con el metodo del recocio
+                //final NeuralSimulatedAnnealing train = new NeuralSimulatedAnnealing(network, xorData, xorIdeal, 10, 2, 1000);
+		final TrainingSetNeuralGeneticAlgorithm train = new TrainingSetNeuralGeneticAlgorithm(network, true, xorData, xorIdeal, 500, 0.7, 0.3);
+                //final Train train = new Backpropagation(network, xorData, xorIdeal,0.7, 0.9);
 		int epoch = 1;
 		do {
-			train.iteration();
-			System.out.println("Epoch #" + epoch + " Error:" + train.getError());
-			epoch++;
-		} while ((epoch < 5000) && (train.getError() > 0.001));
+			try {
+                            train.iteration();
+                            System.out.println("Epoch #" + epoch + " Error:" + train.getError());
+                            epoch++;
+                        }
+                        catch(Exception e) {
+                            System.out.println(e);
+                        }
+                        
+		} while ((epoch < 500) && (train.getError() > 0.001));
+                
+                //Entrenar con algoritmo genetico
+                
+                
 		System.out.println("Resultados de la red:");
                 for (int i = 0; i < xorIdeal.length -1; i++) {
-			final double actual[] = network.computeOutputs(xorData[i]);
+			final double actual[] = network.computeOutputs(xorData2[i]);
 			if ((actual[0] >= 0) && (actual[0] <= 0.33)){
-                           System.out.println(xorData[i][0] + "," + xorData[i][1]+ "," + xorData[i][2]+ "," + xorData[i][3]+ "," + xorData[i][4]+ "," + xorData[i][5]
-					+ ", actual=" + actual[0] + ",ideal=" + xorIdeal[i][0] + ",Su recomendación es: Disney XD");
+                           System.out.println(xorData2[i][0] + "," + xorData2[i][1]+ "," + xorData2[i][2]+ "," + xorData2[i][3]+ "," + xorData2[i][4]+ "," + xorData2[i][5]
+					+ ", actual=" + actual[0] + " ,Su recomendación es: Disney XD");
                         }else if((actual[0] > 0.33) && (actual[0] <= 0.66)){
-                        System.out.println(xorData[i][0] + "," + xorData[i][1]+ "," + xorData[i][2]+ "," + xorData[i][3]+ "," + xorData[i][4]+ "," + xorData[i][5]
-					+ ", actual=" + actual[0] + ",ideal=" + xorIdeal[i][0] + ",Su recomendación es: Cartoon Network");
+                        System.out.println(xorData2[i][0] + "," + xorData2[i][1]+ "," + xorData2[i][2]+ "," + xorData2[i][3]+ "," + xorData2[i][4]+ "," + xorData2[i][5]
+					+ ", actual=" + actual[0] + ",Su recomendación es: Cartoon Network");
                         }else if((actual[0] > 0.66) && (actual[0] <= 1)){
-                        System.out.println(xorData[i][0] + "," + xorData[i][1]+ "," + xorData[i][2]+ "," + xorData[i][3]+ "," + xorData[i][4]+ "," + xorData[i][5]
-					+ ", actual=" + actual[0] + ",ideal=" + xorIdeal[i][0] + ",Su recomendación es: Nick Classic");
+                        System.out.println(xorData2[i][0] + "," + xorData2[i][1]+ "," + xorData2[i][2]+ "," + xorData2[i][3]+ "," + xorData2[i][4]+ "," + xorData2[i][5]
+					+ ", actual=" + actual[0] + ",Su recomendación es: Nick Classic");
                         }
                         
 		}
@@ -85,6 +120,20 @@ public class XOR {
             array[i][3] = csv[i][3];
             array[i][4] = csv[i][4];
             array[i][5] = csv[i][5];
+        }
+        return array;
+    }
+        
+        //guarda matriz sin el ideal
+	static double[][] getGrid3() {
+        final double array[][] = new double[filas][6];
+        for (int i = 0; i < filas - 1; i++) {
+            array[i][0] = csv2[i][0];
+            array[i][1] = csv2[i][1];
+            array[i][2] = csv2[i][2];
+            array[i][3] = csv2[i][3];
+            array[i][4] = csv2[i][4];
+            array[i][5] = csv2[i][5];
         }
         return array;
     }
